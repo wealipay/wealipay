27d65c26f5ac2d237988768b0a69c167f6a2feb7
wealipay <1293600794@qq.com>
2021-05-17 18:39:07 +0800

    build

--- src/views/home/child/TplFive.vue
+++ src/views/home/child/TplFive.vue
@@ -7,17 +7,27 @@
             <span
               class="Themefont Themeborder relative overflow fl"
               data-design-rel="new-product"
-            >最新产品</span>
-            <a title="更多" href="Product-index.html" class="fr seeMore Themefonthover">更多&gt;&gt;</a>
+              >最新产品</span
+            >
+            <a
+              title="更多"
+              href="Product-index.html"
+              class="fr seeMore Themefonthover"
+              >更多&gt;&gt;</a
+            >
           </div>
           <div class="row">
             <ul class="product-box clearfix">
               <li class="Animation">
                 <div class="thumbnail">
-                  <a title href="/Product-detail-id-571311.html" class="ImgBox Themefonthover">
+                  <a
+                    title
+                    href="/Product-detail-id-571311.html"
+                    class="ImgBox Themefonthover"
+                  >
                     <div class="div-img">
                       <img
-                        src="http://pic.files.mozhan.com/mozhan/20170710/d14bf3a46b11c1e8cc39dffd9dec34d9.jpg?x-oss-process=image/resize,limit_0,m_fill,w_450,h_338"></div>
+                        src="https://gitee.com/wealipay/data/raw/master/images/swiper/banner1.jpg"
                       />
                     </div>
                     <h5 class="overflow">洪水预警</h5>
@@ -26,30 +36,51 @@
               </li>
               <li class="Animation">
                 <div class="thumbnail">
-                  <a title href="Product-detail-id-571309.html" class="ImgBox Themefonthover">
-                    <div
-                      class="div-img"
-                    ><img alt="默克尔" src="http://pic.files.mozhan.com/mozhan/20170710/5b6a28de3e4e33161cdae3d22dd648fa.jpg?x-oss-process=image/resize,limit_0,m_fill,w_450,h_338"></div>
+                  <a
+                    title
+                    href="Product-detail-id-571309.html"
+                    class="ImgBox Themefonthover"
+                  >
+                    <div class="div-img">
+                      <img
+                        alt="默克尔"
+                        src="https://gitee.com/wealipay/data/raw/master/images/swiper/banner1.jpg"
+                      />
+                    </div>
                     <h5 class="overflow">默克尔</h5>
                   </a>
                 </div>
               </li>
               <li class="Animation">
                 <div class="thumbnail">
-                  <a title href="/Product-detail-id-571308.html" class="ImgBox Themefonthover">
-                    <div
-                      class="div-img"
-                    ><img alt="香港交易" src="http://pic.files.mozhan.com/mozhan/20170710/94905b0addf516807b37a661bb433339.jpg?x-oss-process=image/resize,limit_0,m_fill,w_450,h_338"></div>
+                  <a
+                    title
+                    href="/Product-detail-id-571308.html"
+                    class="ImgBox Themefonthover"
+                  >
+                    <div class="div-img">
+                      <img
+                        alt="香港交易"
+                        src="https://gitee.com/wealipay/data/raw/master/images/swiper/banner1.jpg"
+                      />
+                    </div>
                     <h5 class="overflow">香港交易</h5>
                   </a>
                 </div>
               </li>
               <li class="Animation">
                 <div class="thumbnail">
-                  <a title href="/Product-detail-id-571298.html" class="ImgBox Themefonthover">
-                    <div
-                      class="div-img"
-                    ><img alt="总理" src="http://pic.files.mozhan.com/mozhan/20170710/f0f4129eed38279ab9b0f1bf64cef09e.jpg?x-oss-process=image/resize,limit_0,m_fill,w_450,h_338"></div>
+                  <a
+                    title
+                    href="/Product-detail-id-571298.html"
+                    class="ImgBox Themefonthover"
+                  >
+                    <div class="div-img">
+                      <img
+                        alt="总理"
+                        src="https://gitee.com/wealipay/data/raw/master/images/swiper/banner1.jpg"
+                      />
+                    </div>
                     <h5 class="overflow">总理</h5>
                   </a>
                 </div>
@@ -70,6 +101,6 @@
 </template>
 <script>
 export default {
-  name:"TplFive"
-}
+  name: "TplFive",
+};
 </script>
