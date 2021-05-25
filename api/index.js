fdd915971ced53b14e6c6023cc797f1b2412d817
wealipay <1293600794@qq.com>
2021-05-25 14:49:07 +0800

    re

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
 module.exports = (req, res) => {
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  })
     const { name = 'World' } = req.query
     res.send(`Hello ${name}!`)
   }
