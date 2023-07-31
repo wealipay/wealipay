05812d34e9e5f65e3b435dd3e6b16f9cc937d855
alipay <1293600794@qq.com>
2023-07-31 01:37:22 +0000

    7.31

--- src/views/tabs/TabsView.vue
+++ src/views/tabs/TabsView.vue
@@ -3,13 +3,22 @@ import { RouterView,useRoute,useRouter } from "vue-router";
 import {Tabbar,TabbarItem} from 'vant'
 import { ref,watch } from "vue";
 
+const route=useRoute()
+const router=useRouter()
+const active=ref(route.name as string)
 
+watch(active,(nv)=>{
+    router.push({
+        name:nv
+    })
+})
 </script>
 
 <template>
+    <RouterView />
     <Tabbar v-model="active">
         <TabbarItem name='home' icon='home-o'>首页</TabbarItem>
-        <TabbarItem name='oder' icon='bars'>订单</TabbarItem>
+        <TabbarItem name='order' icon='bars'>订单</TabbarItem>
         <TabbarItem name="me" icon="contact">我的</TabbarItem>
     </Tabbar>
 </template>
