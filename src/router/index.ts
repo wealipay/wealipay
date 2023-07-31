53ff336520e8ac370ae07abd39c261d4152b9704
alipay <1293600794@qq.com>
2023-07-31 06:02:40 +0000

    vercel

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ import OrderView from '@/views/tabs/order/OrderView.vue'
 
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
