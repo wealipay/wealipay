d9651a66f460ffb7f866604b15ff2bafb65f3133
wealipay <72676183+wealipay@users.noreply.github.com>
2025-04-23 10:22:40 +0800

    Update next.config.ts
    dddd

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   productionBrowserSourceMaps: true, 
+  experimental: {
+    outputFileTracingIncludes: {
+      /* 示例：包含 src 目录下所有文件（根据实际需求调整）*/
+      '/app/**/*': ['./app/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
